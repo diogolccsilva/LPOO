ca0fc5e70af0b2df4118bb2f07d4353f9cd346fc
Vasco Pereira <vasco_mag_pereira@hotmail.com>
2016-05-21 11:17:46 +0100

    Synchronize the hero
    Hero's change of state is synchronized with the animations

--- Project/core/src/com/lpoo/project/logic/Hero.java
+++ Project/core/src/com/lpoo/project/logic/Hero.java
@@ -17,6 +17,7 @@ import java.util.LinkedList;
 public class Hero extends Character {
 
     public enum HeroStatus { STILL, ATTACK, MOVE_LEFT, MOVE_RIGHT, DEAD }
+    private int move_speed, attack_speed;
     private HeroStatus state;
     private HeroStatus nextState;
     private int stateTime;
@@ -35,4 +36,45 @@ public class Hero extends Character {
         super(name, x, y, health, resistance, strength );
     }
 
+    public void attack( ) {
+        nextState = HeroStatus.ATTACK;
+    }
+
+    public void move( int dir ) {
+        if( dir < 0 )
+            nextState = HeroStatus.MOVE_LEFT;
+        else
+            nextState = HeroStatus.MOVE_RIGHT;
+    }
+
+    public void update( int delta ) {
+        int speed = 0;
+
+        if( state == HeroStatus.STILL ) {
+            if( nextState != HeroStatus.STILL ) {
+                stateTime = 0;
+                state = nextState;
+                nextState = HeroStatus.STILL;
+            }
+            return ;
+        }
+
+        stateTime += delta;
+
+        switch ( state ) {
+            case ATTACK:
+                speed = attack_speed;
+                break;
+            case MOVE_LEFT:
+            case MOVE_RIGHT:
+                speed = move_speed;
+                break;
+        }
+
+        if( stateTime > speed ) {
+            stateTime = 0;
+            state = nextState;
+            nextState = HeroStatus.STILL;
+        }
+    }
 }
