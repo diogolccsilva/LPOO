d798da1bce2403bf99afe0394e347e788e919b1b
Vasco Pereira <vasco_mag_pereira@hotmail.com>
2016-06-07 21:05:02 +0100

    Icons and name changed on Android

--- LPOO2/desktop/src/com/lpoo/project/desktop/DesktopLauncher.java
+++ LPOO2/desktop/src/com/lpoo/project/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.lpoo.project.MyGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "AnimalFlip";
+		config.width = 453;
+		config.height = 256;
 		new LwjglApplication(MyGame.getInstance(), config);
 	}
 }
